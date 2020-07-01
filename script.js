fe2c8607c7caa52d66463aeb7e9a919d94d4145c
nadya <nadya.wyn@ya.ru>
2020-07-01 13:58:03 +0500

    Added final files

--- script.js
+++ script.js
@@ -5,4 +5,56 @@ let slides = document.querySelectorAll('.slider__item'),
 	prev = document.querySelector('.prev'),
 	next = document.querySelector('.next'),
 	dotsWrap = document.querySelector('.slider__dots'),
-	dot = document.querySelectorAll('.slider__dot');
+	dots = document.querySelectorAll('.slider__dot');
+
+//Function to show the active slide and hide all others:
+function showSlides(n) {
+	// Making sure the slides will switch from the last to the 1st one and vise versa
+	if (n > slides.length) {
+		slideIndex = 1;
+	}
+	if (n < 1) {
+		slideIndex = slides.length;
+	}
+
+	slides.forEach((item) => {
+		item.style.display = 'none'; // Add display:none to all the slider__items
+	});
+
+	dots.forEach((item) => { // Remove class active from all the slider__dots
+		item.classList.remove('dot-active');
+	});
+
+	slides[slideIndex - 1].style.display = 'block'; // Show the current slider__item
+	dots[slideIndex - 1].classList.add('dot-active'); // Make the current slider__dot active
+}
+
+showSlides(slideIndex);
+
+function slidePlus(n) { // Function to switch to the next / previous slide
+	showSlides(slideIndex += n);
+}
+
+function slideCurrent(n) { // Function to swich to the slide corresponding the dot
+	showSlides(slideIndex = n);
+}
+
+// Connect functions to click events on PREV & NEXT arrows
+
+prev.addEventListener('click', () => {
+	slidePlus(-1); // Into the argument 'n' we put a negative 1 in order to reduce the slideIndex by 1
+});
+
+next.addEventListener('click', () => {
+	slidePlus(1);
+});
+
+// Connect function to click event on the DOTS
+
+dotsWrap.addEventListener('click', (e) => {
+	for (let i = 0; i < dots.length + 1; i++) {
+		if (e.target.classList.contains('slider__dot') && e.target == dots[i - 1]) {
+			slideCurrent(i);
+		}
+	}
+});
